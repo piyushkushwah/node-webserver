4589a1e452f8e84743845ab6d0ced106fe650a72
piyush kushwah <kushwahpiyush4@gmail.com>
2018-06-08 10:40:42 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var date = new Date().toDateString();
+    var log = `${date}:${req.method}${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('unable to append to serer.log');
+        }
+    });
+    next();
+    
+});
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+
+
+});
+app.use(express.static(__dirname+'/public'));
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text;
+});
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+      page:'Home Page',
+      currentYear:new Date().getFullYear(),
+      welcomeMessage:'welcome to my website'
+    });
+});
+
+
+
+
+
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        page:'About Page',
+        currentYear:new Date().getFullYear()
+
+    });
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({bad:'Cannot GET'});
+});
+
+app.listen(3000,()=>{
+    console.log('server is up on port 3000');
+});
